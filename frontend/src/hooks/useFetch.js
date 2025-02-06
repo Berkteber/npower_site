2c3e291816e3a9d6e7296e30187d36f76a970b41
Berkteber <berktbrnecati@gmail.com>
2025-02-06 14:20:31 +0300

    useFetch hookunu REACT_APP_API_URL ile güncelledim2

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,27 +1,27 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-function useFetch(endpoint, baseUrl = process.env.REACT_APP_API_URL + "/api") {
+function useFetch(endpoint, baseUrl= 'https://npowersitecms-production.up.railway.app/api/') {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
-    const api = process.env.REACT_APP_API_URL; // API URL'yi dışarı aktarmak için
+    const api = "https://npowersitecms-production.up.railway.app"
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.get(`${baseUrl}${endpoint}`);
                 setData(response.data.data);
-            } catch (err) {
+            } catch (err){
                 setError(err);
-            } finally {
+            }finally{
                 setLoading(false);
             }
         };
         fetchData();
     }, [endpoint, baseUrl]);
 
-    return { data, loading, error, api };
+    return{data, loading, error, api}
 }
 
 export default useFetch;
